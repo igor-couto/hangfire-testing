63a401d9c1dcc05367bd9fc7c6c4fd91ad7886eb
Igor Couto <igor.fcouto@gmail.com>
2021-03-20 13:49:49 -0300

    Project Upload

---
+++ api/Controllers/FireAndForgetController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FireAndForgetController : ControllerBase
+    {
+        private readonly ILogger<FireAndForgetController> _logger;
+
+        public FireAndForgetController(ILogger<FireAndForgetController> logger)
+            => _logger = logger;
+        
+        [HttpPost]
+        [Route("test")]
+        public IActionResult Post()
+        {
+            DoWork();
+            return Ok();
+        }
+
+        public void DoWork() 
+            => _logger.LogInformation("\nWork Done!");
+    }
+}
---
+++ client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:5001")
+            };
+            var stopwatch = new Stopwatch();
+
+            Console.WriteLine("Starting requests:\n");
+
+            while(true)
+            {
+                stopwatch.Start();
+                var response = await httpClient.PostAsync("fireandforget/test", default);
+                stopwatch.Stop();
+
+                Console.WriteLine($"Request result: \n{response.StatusCode} - {response.ReasonPhrase} \nTime: {stopwatch.Elapsed.TotalMilliseconds} milliseconds\n\n");
+                stopwatch.Reset();
+            }
+        }
+    }
+}
