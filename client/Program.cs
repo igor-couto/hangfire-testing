745c050439fe8a3ceeae95055b9b49a6d223288e
Igor Couto <igor.fcouto@gmail.com>
2021-03-20 15:17:52 -0300

    Create average time tests

--- client/Program.cs
+++ client/Program.cs
@@ -9,6 +9,15 @@ namespace client
     {
         static async Task Main(string[] args)
         {
+            var numberOfRequestsToBeDone = 50;
+
+            if (args.Length > 0) 
+                numberOfRequestsToBeDone = int.Parse(args[0]);
+
+            var averageSyncTime = 0d;
+            var averageAsyncTime = 0d;
+            var averageHangfireTime = 0d;
+
             var httpClient = new HttpClient
             {
                 BaseAddress = new Uri("https://localhost:5001")
@@ -17,15 +26,46 @@ namespace client
 
             Console.WriteLine("Starting requests:\n");
 
-            while(true)
+            for(var i = 0; i < numberOfRequestsToBeDone; i++)
             {
                 stopwatch.Start();
-                var response = await httpClient.PostAsync("fireandforget/test", default);
+                var response = await httpClient.PostAsync("fireandforget/syncronous", default);
                 stopwatch.Stop();
 
-                Console.WriteLine($"Request result: \n\t{(int)response.StatusCode} - {response.ReasonPhrase} \n\tTime: {stopwatch.Elapsed.TotalMilliseconds} milliseconds\n\n");
+                Console.WriteLine($"Request result: \n\t{(int)response.StatusCode} - {response.ReasonPhrase} \n\tTime: {stopwatch.Elapsed.TotalMilliseconds} milliseconds\n");
+                averageSyncTime += stopwatch.Elapsed.TotalMilliseconds;
                 stopwatch.Reset();
             }
+            averageSyncTime = averageSyncTime / numberOfRequestsToBeDone;
+
+            for (var i = 0; i < numberOfRequestsToBeDone; i++)
+            {
+                stopwatch.Start();
+                var response = await httpClient.PostAsync("fireandforget/asyncronous", default);
+                stopwatch.Stop();
+
+                Console.WriteLine($"Request result: \n\t{(int)response.StatusCode} - {response.ReasonPhrase} \n\tTime: {stopwatch.Elapsed.TotalMilliseconds} milliseconds\n");
+                averageAsyncTime += stopwatch.Elapsed.TotalMilliseconds;
+                stopwatch.Reset();
+            }
+            averageAsyncTime = averageAsyncTime / numberOfRequestsToBeDone;
+
+            for (var i = 0; i < numberOfRequestsToBeDone; i++)
+            {
+                stopwatch.Start();
+                var response = await httpClient.PostAsync("fireandforget/hangfire", default);
+                stopwatch.Stop();
+
+                Console.WriteLine($"Request result: \n\t{(int)response.StatusCode} - {response.ReasonPhrase} \n\tTime: {stopwatch.Elapsed.TotalMilliseconds} milliseconds\n");
+                averageHangfireTime += stopwatch.Elapsed.TotalMilliseconds;
+                stopwatch.Reset();
+            }
+            averageHangfireTime = averageHangfireTime / numberOfRequestsToBeDone;
+
+            Console.WriteLine("TEST RESULT:");
+            Console.WriteLine($"\tAverage Syncronous Time: {averageSyncTime}");
+            Console.WriteLine($"\tAverage Asyncronous Time: {averageAsyncTime}");
+            Console.WriteLine($"\tAverage Hangfire Time: {averageHangfireTime}");
         }
     }
 }
